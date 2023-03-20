9d0e264d371a0469f60c8d0cf7339733548328bc
Hedgebzh <axel.gore35@gmail.com>
2023-03-20 16:46:43 +0100

    First push

---
+++ ASGT_engine/authentication/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib import admin
+from authentication.models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password'
+    )
+    
+admin.site.register(User, UserAdmin)
+
+
+
+
+
+
+
+
---
+++ ASGT_engine/authentication/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from . import forms
+from django.conf import settings
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('homepage')
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('homepage')
+            else :
+                message = 'Identifiants invalides.'
+                
+    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+
