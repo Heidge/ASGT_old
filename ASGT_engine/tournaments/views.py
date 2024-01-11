31ee6f07f3772d6bb380012bca1b7bc541f45bf1
Axel Goré <axel.gore@outlook.fr>
2024-01-11 17:09:56 +0100

    Changes in tournament_create function in views.py

--- ASGT_engine/tournaments/views.py
+++ ASGT_engine/tournaments/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from tournaments.models import Tournament, User, Games
+from tournaments.models import Tournament, User, Games, Round1
 from tournaments.forms import ContactUsForm, TournamentForm
 from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+import random
 import json
+
+# l'object request est une convention et est un objet HttpRequest. Voir les méthodes sur la doc django
+
+
 
 
 def homepage(request):
@@ -65,10 +70,23 @@ def tournament_detail(request, tournament_id):
 
 @login_required
 def tournament_create(request):
+    i = 0
     if request.method == 'POST':
         tournament_form = TournamentForm(request.POST)
+        round1_tournament = Round1()
+        tournament_players = [tournament_form.player1, tournament_form.player2, tournament_form.player3, tournament_form.player4, tournament_form.player5, tournament_form.player6, tournament_form.player7, tournament_form.player8]
+
+        #création d'une liste des joueurs 
         if tournament_form.is_valid():
             tournament_form = tournament_form.save()
+            round_players = [round1_tournament.player1, round1_tournament.player2, round1_tournament.player3, round1_tournament.player4, round1_tournament.player5, round1_tournament.player6, round1_tournament.player7, round1_tournament.player8]
+
+            #tant que la liste tournament_player n'est pas nulle, on ajoute en random les joueurs dans round_player
+            while len(tournament_players) > 0:
+                round_players[i] = random.choice(tournament_players)
+                tournament_players.remove(round_players[i])
+                i += 1
+            round1_tournament.save()
             return redirect('tournament-detail', tournament_form.id)
     else:
         tournament_form = TournamentForm()
